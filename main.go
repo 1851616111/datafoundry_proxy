101bb1008362a062bb8c673ef8478c9095adfbc8
zonesan <san@zoobook.org>
2016-05-26 17:29:35 +0800

    login to gitlab when successfully login into apiserver, due to getting private token on gitlab.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -21,6 +22,11 @@ func (p *waylandMux)NotFoundHandler()http.Handler{
     return http.HandlerFunc(NotFoundw)
 }
 */
+
+var apiserver = "https://dev.dataos.io:8443/oauth/authorize?client_id=openshift-challenging-client&response_type=token"
+var oauthurl = "https://datafoundry-oauth.app.dataos.io/v1/repos/gitlab/login"
+var gitlaburl = "https://code.dataos.io"
+
 func (p *waylandMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("from", r.RemoteAddr, r.Method, r.URL.RequestURI(), r.Proto)
@@ -45,6 +51,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 			token, status := token_proxy(auth)
 			if len(token) > 0 {
 				log.Println(token)
+
 				resphttp(w, http.StatusOK, []byte(token))
 			} else {
 				resphttp(w, status, nil)
@@ -63,7 +70,46 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 }
 
-var url = "https://54.222.199.235:8443/oauth/authorize?client_id=openshift-challenging-client&response_type=token"
+func logingitlab(basic string, auth map[string]string) {
+	//log.Println(basic, auth)
+	var bearer, posturl string
+	if len(auth["token_type"]) == 0 || len(auth["access_token"]) == 0 {
+		log.Println(auth, "doesn't contain a complete token")
+	} else {
+		bearer = auth["token_type"] + " " + auth["access_token"]
+	}
+
+	b64auth := strings.Split(basic, " ")
+	if len(b64auth) != 2 {
+		log.Println("basic string error.")
+		return
+	} else {
+		payload, _ := base64.StdEncoding.DecodeString(b64auth[1])
+		pair := strings.Split(string(payload), ":")
+		if len(pair) != 2 {
+			log.Println(pair, "doesn't contain a username or password.")
+			return
+		} else {
+			posturl = fmt.Sprintf("%s?host=%s&username=%s&password=%s", oauthurl, gitlaburl, pair[0], pair[1])
+		}
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	req, _ := http.NewRequest("POST", posturl, nil)
+	req.Header.Set("Authorization", bearer)
+	//log.Println(req.Header, bearer)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		log.Println(req.Host, req.Method, req.URL.RequestURI(), req.Proto, resp.StatusCode)
+	}
+	return
+}
 
 func token_proxy(auth string) (token string, status int) {
 	//fmt.Println("prepear to get token from", url, "with", auth)
@@ -75,7 +121,7 @@ func token_proxy(auth string) (token string, status int) {
 
 	var DefaultTransport http.RoundTripper = tr
 
-	req, _ := http.NewRequest("HEAD", url, nil)
+	req, _ := http.NewRequest("HEAD", apiserver, nil)
 	req.Header.Set("Authorization", auth)
 
 	resp, err := DefaultTransport.RoundTrip(req)
@@ -91,6 +137,7 @@ func token_proxy(auth string) (token string, status int) {
 			m := strings.Split(url.Fragment, "&")
 			n := proc(m)
 			r, _ := json.Marshal(n)
+			go logingitlab(auth, n)
 			return string(r), resp.StatusCode
 		}
 	}
@@ -126,9 +173,21 @@ func main() {
 }
 
 func init() {
-	apiserver := os.Getenv("DATAFOUNDRY_APISERVER_ADDR")
-	if len(apiserver) > 0 {
-		url = "https://" + apiserver + "/oauth/authorize?client_id=openshift-challenging-client&response_type=token"
+	api := os.Getenv("DATAFOUNDRY_APISERVER_ADDR")
+	if len(api) > 0 {
+		apiserver = "https://" + api + "/oauth/authorize?client_id=openshift-challenging-client&response_type=token"
 	}
-	log.Println(url)
+
+	oauthserver := os.Getenv("DATAFOUNDRY_OAUTH_URL")
+	if len(oauthserver) > 0 {
+		oauthurl = oauthserver
+	}
+
+	gitserver := os.Getenv("DATAFOUNDRY_GIT_ADDR")
+	if len(gitserver) > 0 {
+		gitlaburl = gitserver
+	}
+	log.Println("apiserver", apiserver)
+	log.Println("oauthurl", oauthurl)
+	log.Println("gitlaburl", gitlaburl)
 }
